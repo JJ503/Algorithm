871aa3bfc0036aa1daa920a6eab44a6e6882d0bd
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-09-04 22:34:04 +0900

    Update [Programmers] 최대공약수와 최소공배수.py

--- Math/[Programmers] 최대공약수와 최소공배수.py
+++ Math/[Programmers] 최대공약수와 최소공배수.py
@@ -3,6 +3,7 @@ def solution(n, m):
     
     multiple = n*m    
     
+    # 최대공약수 : 유클리드 호제법 사용
     if n < m :
         tmp = n
         n = m
@@ -14,6 +15,6 @@ def solution(n, m):
         m = c
     
     answer.append(n)
-    answer.append(multiple / n)
+    answer.append(multiple / n)  # 최소공배수 (두 값 곱) / (최대공약수)
     
     return answer
