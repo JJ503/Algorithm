b589c4ed2d6a8ec888357b9f7ccf2c5d6a611f69
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-09-10 00:35:38 +0900

    Update [Leetcode] 3. Longest Substring Without Repeating Characters.py

--- Hash/[Leetcode] 3. Longest Substring Without Repeating Characters.py
+++ Hash/[Leetcode] 3. Longest Substring Without Repeating Characters.py
@@ -1,22 +1,17 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         answer = 0       
-        strDict = dict()
 
         for i in range(len(s)):
             temp = ""
             for j in range(i, len(s)):
                 if temp == "":
                     temp = s[j]
-                    strDict[temp] = len(temp)
                 elif s[j] in temp:
                     temp = s[j]
-                    strDict[temp] = len(temp)
                     break
                 else:
-                    del strDict[temp]
                     temp += s[j]
-                    strDict[temp] = len(temp)
 
                 if answer < len(temp):
                     answer = len(temp)
