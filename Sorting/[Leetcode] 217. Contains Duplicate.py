b25d0880347eb0cbff515eefb9f5c78d37c89843
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-09-15 11:55:24 +0900

    Create [Leetcode] 217. Contains Duplicate.py

---
+++ Sorting/[Leetcode] 217. Contains Duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        setNums = set(nums)
+        
+        if len(setNums) == len(nums):
+            return False
+        else:
+            return True
