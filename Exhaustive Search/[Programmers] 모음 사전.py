1d08f8bf2596b10b0e3551386894974206041e40
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-10-10 23:55:28 +0900

    Update [Programmers] 모음 사전.py

--- Exhaustive Search/[Programmers] 모음 사전.py
+++ Exhaustive Search/[Programmers] 모음 사전.py
@@ -4,25 +4,13 @@ def solution(word):
     answer = 0
     alphas = ['A', 'E', 'I', 'O', 'U']
     
-    for alpha in alphas:
-        if alpha == word[0]:
-            break
-        
+    for i in range(0, len(word)):
         answer += 1
-        for i in range(1, 5):
-            answer += int(math.pow(5, i))
-            
-    answer += 1      
-    # print(answer)
-    if len(word) > 1:
-        for i in range(1, len(word)):
-            answer += 1
-            for alpha in alphas:
-                if alpha == word[i]:
-                    break
-                    
-                answer += 1
-                for j in range(1, 5 - i):
-                    answer += int(math.pow(5, j))
+        for alpha in alphas:
+            if alpha == word[i]:
+                break
+
+            for j in range(0, 5 - i):
+                answer += int(math.pow(5, j))
                     
     return answer
