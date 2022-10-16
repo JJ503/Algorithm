4a45ea39810cc4bb6556fc60fad6652bf23689d4
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-10-16 23:46:37 +0900

    Create [Leetcode] 258. Add Digits.py

---
+++ Implementation/[Leetcode] 258. Add Digits.py
@@ -0,0 +1,14 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        
+        while True:
+            tempList = list(str(num))
+            
+            if len(tempList) == 1:
+                break
+                
+            tempList = map(int, tempList)
+            num = sum(tempList)
+            
+            
+        return num
