1743e04eadf9940264ae836722369d89becf821c
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-01-15 14:29:18 +0900

    Change Folder String Category to Programmers
    String 카테고리의 파일들을 Programmers의 레벨별 분류로 변경

---
+++ Programmers/lv 2/[Programmers] JadenCase 문자열 만들기.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = ''
+    
+    wordList = s.lower().split(' ')
+    answerList = []
+        
+    for i in range(len(wordList)):
+        if wordList[i] != '':
+            answerList.append(wordList[i][0].upper() + wordList[i][1:].lower())
+        else:
+            answerList.append(wordList[i])
+        
+    
+    answer = " ".join(answerList)
+    
+    return answer
