ba3172a4418bd18f0fba31b8ea797ac9c56157ea
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-02-01 00:10:37 +0900

    Update [Programmers] 다음 큰 숫자.py
    정답 이전 버전을 업로드해 수정함

--- Programmers/lv 2/[Programmers] 다음 큰 숫자.py
+++ Programmers/lv 2/[Programmers] 다음 큰 숫자.py
@@ -3,20 +3,15 @@ def solution(n):
     
     binStr = bin(n)[2:]
     binStrList = list(binStr)
+    binStrList.insert(0, "0")
     
-    if binStrList.count("1") == len(binStrList):
-        binStrList.insert(1, "0")
-        binStr = "0b" + "".join(binStrList)
-        return int(binStr, 2)
-    
-    countOne = 0
     fixOne = -1
     for i in range(len(binStrList) - 1, 0, -1):        
-        if binStrList[i] == "1" and binStrList[i - 1] == "1":
-            countOne += 1
-        elif binStrList[i] == "1" and binStrList[i - 1] == "0":
+        if binStrList[i] == "1" and binStrList[i - 1] == "0":
             fixOne = i
-    
+            break
+
+    countOne = binStrList[fixOne+1:].count("1")
     answerList = ["1" for _ in range(countOne)]
     for i in range(len(binStrList) - countOne - 1, -1, -1):
         if i == fixOne - 1:
@@ -29,5 +24,6 @@ def solution(n):
             answerList.insert(0, binStrList[i])
     
     binStr = "0b" + "".join(answerList)
+    answer = int(binStr, 2)
     
-    return int(binStr, 2)
+    return answer
