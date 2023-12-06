8f10a9a2a4a4db10371cedb5142b10231262d68e
제이미 <63184334+JJ503@users.noreply.github.com>
2023-12-06 17:07:21 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -2,14 +2,10 @@ def solution(s):
     answer = ''
     
     sList = s.split(' ')
-    print(sList)
-    
     sList = [int(s) for s in sList]
-    print(sList)
     
     answer += str(min(sList))
     answer += " "
     answer += str(max(sList))
-    print(answer)
     
     return answer
