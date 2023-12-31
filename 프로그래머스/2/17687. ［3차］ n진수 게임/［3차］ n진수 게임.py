aa550b43b37e213ed4f2d3ad48b71aed8c62c851
제이미 <63184334+JJ503@users.noreply.github.com>
2023-12-31 23:29:46 +0900

    [level 2] Title: [3차] n진수 게임, Time: 2.87 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -0,0 +1,20 @@
+def solution(n, t, m, p):
+    answer = '0'
+    
+    letter = "0123456789ABCDEF"
+    
+    currentNumber = -1
+    while True:
+        currentNumber += 1
+        if len(answer) >= t * m:
+            break
+        
+        temp = ''
+        number = currentNumber
+        while number > 0:
+            number, mod = divmod(number, n)
+            temp = letter[mod] + temp
+        
+        answer += temp
+    
+    return answer[p-1::m][:t]
