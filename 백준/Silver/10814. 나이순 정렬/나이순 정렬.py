1a9f7ef564808d239c0684a105f8ad8354675521
제이미 <63184334+JJ503@users.noreply.github.com>
2024-02-03 15:15:03 +0900

    [Silver V] Title: 나이순 정렬, Time: 4468 ms, Memory: 49332 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,11 @@
+import heapq
+
+n = int(input())
+array = []
+for i in range(n):
+    age, name = input().split()
+    heapq.heappush(array, (int(age), i, name))
+
+while array:
+    age, i, name = heapq.heappop(array)
+    print(age, name)
