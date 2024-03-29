a21a43f734d1dcd6c62e40f910730e0f889d78fe
제이미 <63184334+JJ503@users.noreply.github.com>
2024-03-29 16:54:49 +0900

    [level 3] Title: 아이템 줍기, Time: 0.48 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+def solution(rectangle, characterX, characterY, itemX, itemY):
+    answer = 0
+
+    maps = [[-1] * 102 for _ in range(102)]
+    for rec in rectangle:
+        x1, y1, x2, y2 = map(lambda x: x * 2, rec)
+        for i in range(x1, x2 + 1):
+            for j in range(y1, y2 + 1):
+                if x1 < i < x2 and y1 < j < y2:
+                    maps[i][j] = 0
+                elif maps[i][j] != 0:
+                    maps[i][j] = 1
+    
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    
+    queue = deque()
+    queue.append([characterX * 2, characterY * 2])
+    visited = [[1] * 102 for _ in range(102)]
+    visited[characterX * 2][characterY * 2] = 0
+    
+    while queue:
+        x, y = queue.popleft()
+        if x == itemX * 2 and y == itemY * 2:
+            answer = visited[x][y] // 2
+            break
+            
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if maps[nx][ny] == 1 and visited[nx][ny] == 1:
+                queue.append([nx, ny])
+                visited[nx][ny] = visited[x][y] + 1
+    
+    return answer
