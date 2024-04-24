245ff242ede0508d144a829ae735d75d86778fa3
제이미 <63184334+JJ503@users.noreply.github.com>
2024-04-25 07:57:38 +0900

    [Silver III] Title: 바이러스, Time: 128 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,44 +1,43 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    static int n, line;
+    static Map<Integer, List<Integer>> maps = new HashMap<>();
     static boolean[] visited;
-    static int[][] array;
-    static int answer = 0;
+    static int answer;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
 
-        n = Integer.parseInt(br.readLine());
-        line = Integer.parseInt(br.readLine());
-
-        array = new int[n + 1][n + 1];
         visited = new boolean[n + 1];
 
-        for (int i = 0; i < line; i++) {
+        for (int i = 0; i < m; i++) {
             StringTokenizer str = new StringTokenizer(br.readLine());
-
-            int a = Integer.parseInt(str.nextToken());
-            int b = Integer.parseInt(str.nextToken());
-
-            array[a][b] = array[b][a] = 1;
+            int firstIndex = Integer.parseInt(str.nextToken());
+            int secondIndex = Integer.parseInt(str.nextToken());
+            maps.putIfAbsent(firstIndex, new ArrayList<Integer>());
+            maps.putIfAbsent(secondIndex, new ArrayList<Integer>());
+            maps.get(firstIndex).add(secondIndex);
+            maps.get(secondIndex).add(firstIndex);
         }
 
         dfs(1);
-
-        System.out.println(answer - 1);
+        System.out.println(answer);
     }
 
-    public static void dfs(int start) {
-        visited[start] = true;
-        answer++;
+    private static void dfs(int idx) {
+        visited[idx] = true;
 
-        for (int i = 1; i <= n; i++) {
-            if (array[start][i] == 1 & !visited[i]) {
-                dfs(i);
+        List<Integer> array = maps.get(idx);
+        if (array != null) {
+            for (Integer nextIdx : array) {
+                if (!visited[nextIdx]) {
+                    answer++;
+                    dfs(nextIdx);
+                }
             }
         }
     }
