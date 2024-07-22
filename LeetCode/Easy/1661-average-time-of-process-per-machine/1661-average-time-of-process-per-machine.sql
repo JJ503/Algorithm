f05a3d68c37681b33bdbd8b1ee29b1349669ed45
제이미 <63184334+JJ503@users.noreply.github.com>
2024-07-22 14:13:52 +0900

    Time: 194 ms (89.73%), Space: 0B (100%) - LeetHub

--- LeetCode/Easy/1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ LeetCode/Easy/1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -2,5 +2,5 @@
 SELECT a1.machine_id AS machine_id, ROUND(AVG(a1.timestamp - a2.timestamp), 3) AS processing_time
 FROM activity a1 JOIN activity a2
 ON a1.machine_id = a2.machine_id AND a1.process_id = a2.process_id
-WHERE a1.activity_type = 'end' AND a2.activity_type = 'start'
+    AND a1.activity_type = 'end' AND a2.activity_type = 'start'
 GROUP BY 1
